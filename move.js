223c89e5b4fd381bfce9cbbb00be1359ecda5068
okayzach <znortyy@gmail.com>
2022-03-28 14:46:43 -0400

    finished homework

--- move.js
+++ move.js
@@ -6,7 +6,57 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        setInterval(function(){ 
+            if (direction === 'west'){
+                x -= 1
+            }
+            if (direction === 'east'){
+                x += 1
+            }
+            if (direction === 'north'){
+                y += 1
+            }
+            if (direction === 'south'){
+                y -= 1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }, 1)
+
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            callback(direction)
+        })
+
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
+
